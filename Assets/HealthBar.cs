802046a1a913774d596b87300a3c350cc9d9185b
Theagns <markus.agnsgaard@hotmail.com>
2022-03-30 12:15:19 +0200

    Healthbar helt done, collide med cube done

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -9,19 +9,31 @@ public class HealthBar : MonoBehaviour
 	public Slider slider;
 	public Gradient gradient;
 	public Image fill;
+    float CurrentHealth;
+    public PlayerControllerScript Player;
+
+	private void Start(){
+		Player = FindObjectOfType<PlayerControllerScript>();
+	}
 
 	public void SetMaxHealth(int health)
 	{
 		slider.maxValue = health;
 		slider.value = health;
-
 		fill.color = gradient.Evaluate(1f);
 	}
 
-    public void SetHealth(int health)
-	{
-		slider.value = health;
 
+    public void Update(){
+		SetHealth();
+    }
+
+    public void SetHealth()
+	{
+        CurrentHealth = Player.Health;
+		slider.value = CurrentHealth;
+        
 		fill.color = gradient.Evaluate(slider.normalizedValue);
+
 	}
 }
--- Assets/healthbarscript.cs
+++ Assets/healthbarscript.cs
@@ -1,35 +1,59 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Player : MonoBehaviour
+// {
+
+// 	public int maxHealth = 100;
+// 	public int currentHealth;
+
+// 	public HealthBar healthBar;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+// 		currentHealth = maxHealth;
+// 		healthBar.SetMaxHealth(maxHealth);
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+// 		if (Input.GetKeyDown(KeyCode.Space))
+// 		{
+// 			TakeDamage(20);
+// 		}
+//     }
+
+// 	void TakeDamage(int damage)
+// 	{
+// 		currentHealth -= damage;
+
+// 		healthBar.SetHealth(currentHealth);
+// 	}
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class healthbarscript : MonoBehaviour
 {
+    private Image HealthBar;
+    public float CurrentHealth;
+    private float MaxHealth = 100;
+    PlayerControllerScript Player;
 
-	public int maxHealth = 100;
-	public int currentHealth;
-
-	public HealthBar healthBar;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-		currentHealth = maxHealth;
-		healthBar.SetMaxHealth(maxHealth);
+    HealthBar =  GetComponent<Image>();
+    Player = FindObjectOfType<PlayerControllerScript>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			TakeDamage(20);
-		}
+    	CurrentHealth = Player.Health;
+    	HealthBar.fillAmount = CurrentHealth / MaxHealth;
     }
-
-	void TakeDamage(int damage)
-	{
-		currentHealth -= damage;
-
-		healthBar.SetHealth(currentHealth);
-	}
 }
